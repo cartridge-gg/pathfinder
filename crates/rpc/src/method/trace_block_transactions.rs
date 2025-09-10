019d566480944f09a11e6bdf006411c61c5bdb55
Ammar Arif <evergreenkary@gmail.com>
2025-09-10 12:30:54 -0400

    fix: test using outdated api

--- crates/rpc/src/method/trace_block_transactions.rs
+++ crates/rpc/src/method/trace_block_transactions.rs
@@ -1023,7 +1023,7 @@ pub(crate) mod tests {
         let context = RpcContext::for_tests().with_storage(storage.clone());
 
         let pre_latest_transactions = vec![
-            fixtures::input::declare(account_contract_address).into_common(context.chain_id),
+            fixtures::input::declare(account_contract_address, None).into_common(context.chain_id),
             fixtures::input::universal_deployer(
                 account_contract_address,
                 universal_deployer_address,
@@ -1168,7 +1168,7 @@ pub(crate) mod tests {
         let context = RpcContext::for_tests().with_storage(storage.clone());
 
         let pre_confirmed_transactions = vec![
-            fixtures::input::declare(account_contract_address).into_common(context.chain_id),
+            fixtures::input::declare(account_contract_address, None).into_common(context.chain_id),
             fixtures::input::universal_deployer(
                 account_contract_address,
                 universal_deployer_address,
